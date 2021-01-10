7d4aa6f2294c9a68337091c76a07f73e27b6bef9
unknown <bulgackoff.denis@yandex.ru>
2021-01-10 22:53:20 +0300

    game_ function add

--- game_list/game.js
+++ game_list/game.js
@@ -1,8 +1,38 @@
-async function getResponse(){
+function imgError (img){
+    img.src= "https://icon-library.com/images/file-icon-size/file-icon-size-19.jpg" 
+    img.onerror=""
+    return true
+}
+
+const arr =[]
+const game_add= document.querySelector('.game')
+
+function add (props) {
+    const div =document.createElement('div')
+    arr.push(props)
+    div.innerHTML=arr
+    game_add.appendChild(div)
+}
+
+(async function getResponse(){
     let response = await fetch('https://gist.githubusercontent.com/Greyewi/e6cfa49e478387a7b878e4430e1f4223/raw/d045a5c2c977cf05d05ae1a4625762e69cc891c8/game_list.json');
     let content = await response.json()
     console.log(content)
 
+    const game_list =document.querySelector('.game_list')
+for(let key in content){
+ 
 
+    game_list.innerHTML += `<div class="block">
+                                <img src="${ content[key].cover }" alt="" onerror="imgError(this)" width="150px" height="210px">
+                                <div><button onclick='add(${JSON.stringify((content[key].name))})'>Добавить</button></div>
+                                <p> ${ content[key].name}</p>
+                                ${content[key].price ? `<p>${content[key].price}рублей</p>`:""}
+                            </div>`   
+                           
+                           
 }
-getResponse();
+})()
+
+
+
